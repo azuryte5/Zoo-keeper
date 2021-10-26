6bce3d604799077607978c41851181e1d102270f
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-10-26 09:00:38 -0400

    Set up PORT

--- server.js
+++ server.js
@@ -55,6 +55,8 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
   }
 
+const PORT = process.env.PORT || 3001;
+
 //Hello get()
 app.get('/api/animals', (req, res) => {
     let results = animals
@@ -66,6 +68,6 @@ app.get('/api/animals', (req, res) => {
     });
 
 
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
   });
